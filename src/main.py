0af74f4b25a893ddce931c3b14b19c10287ff399
Steve Violich <stephenviolich@gmail.com>
2024-11-26 18:24:05 +0000

    WIP rough counting is working, still needs special characters and file handling.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,35 @@
-def main(a, b):
-    c = add(a, b)
-    print(c)
+import re
+
+# def main(a, b):
+#     c = add(a, b)
+#     print(c)
+
+# if __name__ == '__main__':
+#     main(2, 4)
 
 
-def add(a, b):
-    return a + b
+# TODO take filepath, not string
+def word_count(text):
+    counts = {}
 
+    # TODO strip punctuation/special chars
 
-if __name__ == '__main__':
-    main(2, 4)
+    # force lowercase
+    text = text.lower()
+
+    # strip leading/trailing whitespace
+    text = text.strip()
+
+    # replace any whitespace with single spaces
+    text = re.sub(r"\s+", " ", text)
+
+    # split text on spaces
+    words = text.split(" ")
+
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+
+    return counts
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,23 @@
 from src import main
 
 
-def test_add():
-    assert main.add(2, 4) == 6
+SAMPLE_TEXT = """
+Hello world!
+This is a sample text file.
+Hello Python world.
+"""
+
+
+def test_word_count():
+    counts = main.word_count(SAMPLE_TEXT)
+    assert counts == {
+        'hello': 2,
+        'world': 2,
+        'this': 1,
+        'is': 1,
+        'a': 1,
+        'sample': 1,
+        'text': 1,
+        'file': 1,
+        'python': 1
+    }
